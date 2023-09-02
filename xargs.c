307beaab9336ba6234debae26836b07a763080c4
César <cessar5@gmail.com>
2023-09-02 17:54:41 -0300

    desarrollo de execvp, fork, wail

--- xargs.c
+++ xargs.c
@@ -6,30 +6,49 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
+
+int
+main(int argc, char *argv[])
 {
-    FILE *stream;
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
+	FILE *stream;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+	if (argc <= 1) {
+		fprintf(stderr, "Usage: %s <file>\n %d", argv[0], argc);
+		exit(EXIT_FAILURE);
+	}
 
-    stream = fopen(argv[1], "r");
-    if (stream == NULL) {
-        perror("fopen");
-        exit(EXIT_FAILURE);
-    }
+	printf("Argumentos: arg0:%s arg1:%s arg2:%s \n", argv[0], argv[1], argv[2]);
 
-    while ((nread = getline(&line, &len, stream)) != -1) {
-        printf("Retrieved line of length %zu:\n", nread);
-        fwrite(line, nread, 1, stdout);
-    }
+	stream = fopen(argv[2], "r");
+	if (stream == NULL) {
+		// perror("fopen");
+		// exit(EXIT_FAILURE);
+		stream = stdin;
+	}
+	char *command = "ls";
+	char *argument_list[] = { "ls", "-l", NULL };
 
-    free(line);
-    fclose(stream);
-    exit(EXIT_SUCCESS);
+	while ((nread = getline(&line, &len, stream)) != -1) {
+		strtok(line, "\n");
+		printf("Retrieved line of length %zu:\n", nread);
+		printf("Retrieved line :%s \n", line);
+
+		if (fork() == 0) {
+			int status_code = execvp(command, argument_list);
+			// si fue satisfactoria la ejuccion de execvp esto no se ejecuta.
+			if (status_code == -1) {
+				printf("execvp termino incorrectamente\n");
+				return 1;
+			}
+		} else {
+			wait(NULL);
+		}
+	}
+
+	free(line);
+	fclose(stream);
+	exit(EXIT_SUCCESS);
 }
